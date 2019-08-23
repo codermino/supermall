1d7c8eccdd9764916b933879e609b333901614c7
mino <1227657064@qq.com>
2019-08-23 23:45:22 +0800

    'supermalllearn'

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,18 @@
 <template>
   <div id="app">
-
+    <router-view></router-view>
+    <main-tar-bar/>
   </div>
 </template>
 
 <script>
+  import MainTarBar from 'components/content/mainTabbar/MainTabBar'
+  import NavBar from 'components/common/tabbar/TabBar'
   export default {
     name: 'app',
     components: {
-
+      MainTarBar,
+      NavBar
     }
   }
 </script>
